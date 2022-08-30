02b81fc9d2efede1a4d93f9c0b980605c9388e1b
Soomin Gim <ghimsoomin@gmail.com>
2022-08-30 16:01:23 +0900

    comit

--- app/api/endpoints/url.py
+++ app/api/endpoints/url.py
@@ -1,10 +1,12 @@
 from app.services.calculator import CalculatorService
+from app.services.titanic import TitanicService
 from app.services.user import UserService
 from app.services.grade import GradeService
 from app.services.quiz import Quiz
-from app.services.m01_01_iris import Iris
+from app.services.ddarung import DDarungService
+from app.services.titanic import TitanicService
 from app.constants.menus import LOGIN, LOGOUT, CALCULATOR, GRADE, \
-    QUIZ_1, QUIZ_2, QUIZ_3, QUIZ_4, IRIS
+    QUIZ_1, QUIZ_2, QUIZ_3, QUIZ_4, DDARUNG, TITANIC
 class Url:
     
     def router(self, menu):
@@ -23,9 +25,15 @@ class Url:
             math = int(input('수학'))
             print(f'이름: {name} \
                 학점: {GradeService().get_grade(name,korean, english, math)}')
+        elif menu == DDARUNG: DDarungService().submit(
+            path='data/ddarung/', train='train.csv', test='test.csv'
+        )
+        elif menu == TITANIC:
+            titanicService = TitanicService()
+            titanicService.submit(
+                path='data/titanic/',train='train.csv', test='test.csv')
         elif menu == QUIZ_1: Quiz().quiz_1()
         elif menu == QUIZ_2: Quiz().quiz_2()
         elif menu == QUIZ_3: Quiz().quiz_3()
         elif menu == QUIZ_4: Quiz().quiz_4()
-        elif menu == IRIS: Iris().exec()
             
--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 basedir = os.path.dirname(os.path.abspath(__file__))
 from app.api.endpoints.url import Url
-from app.constants.menus import LOGIN, LOGOUT, CALCULATOR, GRADE, \
-    QUIZ_1, QUIZ_2, QUIZ_3, QUIZ_4, QUIZ_5, QUIZ_6, IRIS
-
+from app.constants.menus import DDARUNG, LOGIN, LOGOUT, CALCULATOR, GRADE, \
+    QUIZ_1, QUIZ_2, QUIZ_3, QUIZ_4, TITANIC
+    
 
 def print_menu():
     print(' ###################')
@@ -13,13 +13,12 @@ def print_menu():
     print(f'로그아웃 : {LOGOUT}')
     print(f'계산기 : {CALCULATOR}')
     print(f'성적표 : {GRADE}') 
+    print(f'따릉이 : {DDARUNG}') 
+    print(f'타이타닉 : {TITANIC}') 
     print(f'퀴즈 1 : {QUIZ_1}') 
     print(f'퀴즈 2 : {QUIZ_2}')
     print(f'퀴즈 3 : {QUIZ_3}')
     print(f'퀴즈 4 : {QUIZ_4}')
-    print(f'퀴즈 5 : {QUIZ_5}')
-    print(f'퀴즈 6 : {QUIZ_6}')
-    print(f'iris : {IRIS}')
     menu = input('메뉴에서 URL을 카피해서 입력하시오\n')
     print(' ###################')
     return menu
